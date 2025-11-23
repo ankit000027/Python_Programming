5dc33426a488c5342bbf8c2deb5a6cc7feb588bb
ankit000027 <sherwoodankit99@gmail.com>
2025-11-23 05:41:48 +0530

    Day 1 for the basics

---
+++ BandNameGenrator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+birth_place=input("What is name of the city that you were born?\n")
+pet_name=input("What is name of your pet?\n")
+print("Your band name will can be "+birth_place+" "+pet_name+".")
