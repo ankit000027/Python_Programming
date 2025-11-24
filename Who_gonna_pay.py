794fca9169f09f57a6626edcc5bca3af62feb4eb
ankit000027 <sherwoodankit99@gmail.com>
2025-11-24 14:26:07 +0530

    Day-3. Sill going good

---
+++ Who_gonna_pay.py
@@ -0,0 +1,7 @@
+import random
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+print(f"The bill will be paid by {random.choice(friends)}")
+
+#num = len(friends)-1
+#print(f"The bill will be paid by {friends[random.randint(0,num)]}")
