1c63d6c5727b03414e4ca8f0b1ac11e67e887e41
ankit000027 <sherwoodankit99@gmail.com>
2025-11-26 20:56:36 +0530

    updated guess the word. Day-4

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,43 +1,87 @@
 import random
-word_list = ["aardvark", "baboon", "camel"]
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+word_list = ["aardvark", "baboon", "camel","soumili"]
 
-chosen_word = random.choice(word_list)
+random_word = random.choice(word_list)
+spaces = ["_"]*len(random_word)
 
 
-life=len(chosen_word)
-display = ["_"]*life
+life = 7
 
+while life>0 and "_" in spaces:
+    guess = input("Guess the letter: ").lower()
 
-while life!=0 and "_" in display:
-    guess_letter = input("Guess the letter: ").lower()
-
-
-    if guess_letter in display:
-        print("Word already found")
-        life -= 1
-        print(f"Wrong..You have {life} chances left")
-
-
-    elif guess_letter in chosen_word :
-        print("Correct!!")
-        for i in range(len(chosen_word)):
-            if chosen_word[i]==guess_letter:
-                display[i]=chosen_word[i]
-        print(display)
-
-
-    else:
+    if guess not in random_word or guess in spaces:
         life-=1
-        print(f"Wrong..You have {life} chances left")
+        print("Incorrect")
+        print(f"*********************(life remaining:{life})*********************")
+        print(stages[life])
+        print("\n",spaces)
 
-if "_" not in display:
-    print("You saved the hangman..")
-    print(f"The word was: "+"".join(display))
+    elif guess in random_word:
+        print("Correct")
+        for i in range(len(random_word)):
+            if guess==random_word[i]:
+                spaces[i]=guess
+        print(spaces)
 
+if life==0:
+    print(f"You lost!! The word was {random_word}")
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
-
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+else:
+    print(f"You won!! The word was {random_word}")
