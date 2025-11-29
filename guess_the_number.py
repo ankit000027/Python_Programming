97c1c97283bf3d54cee0e986d15ee58dd88bf9cf
ankit000027 <sherwoodankit99@gmail.com>
2025-11-29 20:10:47 +0530

    Updated a bit

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,29 +1,38 @@
 import random
 
-answer = random.randint(1,100)
-print(answer)
-easy_hard = input("Easy or Hard?").lower()
+answer = random.randint(1, 100)
+print(answer)  # Remove this after testing
 
-def guess(num):
-    for i in range(num):
-        user_answer = int(input(f"You have {num-i} attempts left, You have to guess a number between 1-100: "))
+easy_hard = input("Easy or Hard? ").lower()
+
+
+def guess(attempts):
+    for i in range(attempts):
+        print(f"You have {attempts - i} attempts left.")
+        user_answer = int(input("Guess a number between 1-100: "))
+
         if user_answer == answer:
-            return user_answer
-        elif user_answer>answer:
-            print("too high")
-        elif user_answer<answer:
-            print("Too low")
+            print("Correct!")
+            return True
+
+        elif user_answer > answer:
+            print("Too high.")
+
+        else:
+            print("Too low.")
+
     return False
 
+
 if easy_hard == "easy":
-    easy_result=guess(10)
-    if easy_result==False:
-        print("Better luck next time")
-    else:
-        print(f"You won the answer was {answer}")
+    result = guess(10)
 elif easy_hard == "hard":
-    hard_result=guess(5)
-    if hard_result==False:
-        print("Better luck next time")
-    else:
-        print(f"You won the answer was {answer}")
+    result = guess(5)
+else:
+    print("Invalid mode!")
+    result = False
+
+if result:
+    print(f"You won! The answer was {answer}")
+else:
+    print(f"Better luck next time! The answer was {answer}")
